b3fcf4253d4fdb3b564118e2293b6861d87197fd
unknown <davi.aires03@aluno.ifce.edu.br>
2025-07-12 23:37:19 -0300

    aprendendo JFrame, criando janelas e botões

--- ex10/src/aventura/br/com/daviaires/MainWindow.java
+++ ex10/src/aventura/br/com/daviaires/MainWindow.java
@@ -1,17 +1,31 @@
 package aventura.br.com.daviaires;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow {
 
     private JFrame window;
 
     public MainWindow() {
+        initialize();
+    }
+
+    private void initialize(){
         window = new JFrame();
+        window.setLayout(new BorderLayout(10,5));
         window.setTitle("Maple Tale");
-        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         window.setSize(800, 500);
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         window.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+        panel.setBackground(Color.black);
+        Button button = new Button("Button");
+        panel.add(button);
+        window.add(panel, BorderLayout.CENTER);
+
     }
 
     public void show(){
